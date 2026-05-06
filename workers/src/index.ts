9e5be6d319ee91d2aa2c03b8e054cd56ba9cc319
mizuidekazuhiro <mizuide.kazuhiro@gmail.com>
2026-05-06 18:21:41 +0900

    Generalize app usage aggregation from Anki to study totals

--- workers/src/index.ts
+++ workers/src/index.ts
@@ -1,6 +1,6 @@
 import type { Env } from "./types";
 import { routeRequest } from "./routes";
-import { aggregateAnkiUsageForTargetDate, getPreviousJstDateFrom } from "./services/app_usage_session_service";
+import { aggregateStudyUsageForTargetDate, getPreviousJstDateFrom } from "./services/app_usage_session_service";
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
@@ -11,8 +11,8 @@ export default {
     const targetDate = getPreviousJstDateFrom(scheduledMs);
     console.log("APP_USAGE_SCHEDULED_AGGREGATION_START", { target_date: targetDate, scheduled_time_ms: scheduledMs });
     try {
-      const aggregate = await aggregateAnkiUsageForTargetDate(env, targetDate);
-      console.log("APP_USAGE_SCHEDULED_AGGREGATION_DONE", { target_date: targetDate, anki_minutes: aggregate.minutes, anki_sessions: aggregate.sessions, anki_last_used_at: aggregate.last });
+      const aggregate = await aggregateStudyUsageForTargetDate(env, targetDate);
+      console.log("APP_USAGE_SCHEDULED_AGGREGATION_DONE", { target_date: targetDate, study_minutes: aggregate.minutes, study_sessions: aggregate.sessions, study_last_used_at: aggregate.last });
     } catch (error) {
       const msg = error instanceof Error ? error.message : String(error);
       console.error("APP_USAGE_SCHEDULED_AGGREGATION_ERROR", { target_date: targetDate, message: msg });
