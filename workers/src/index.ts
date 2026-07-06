334f27a7b54175961a12123866506c84d2dc4228
mizuidekazuhiro <mizuide.kazuhiro@gmail.com>
2026-07-06 10:23:06 +0900

    Use 3am day start in scheduled app usage aggregation

--- workers/src/index.ts
+++ workers/src/index.ts
@@ -1,6 +1,6 @@
 import type { Env } from "./types";
 import { routeRequest } from "./routes";
-import { aggregateStudyUsageForTargetDate, getPreviousJstDateFrom } from "./services/app_usage_session_service";
+import { aggregateStudyUsageForTargetDate, getAppUsageDayStartHour, getPreviousJstDateFrom } from "./services/app_usage_session_service";
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
@@ -8,14 +8,15 @@ export default {
   },
   async scheduled(controller: ScheduledController, env: Env): Promise<void> {
     const scheduledMs = controller.scheduledTime || Date.now();
-    const targetDate = getPreviousJstDateFrom(scheduledMs);
-    console.log("APP_USAGE_SCHEDULED_AGGREGATION_START", { target_date: targetDate, scheduled_time_ms: scheduledMs });
+    const dayStartHour = getAppUsageDayStartHour(env);
+    const targetDate = getPreviousJstDateFrom(scheduledMs, dayStartHour);
+    console.log("APP_USAGE_SCHEDULED_AGGREGATION_START", { target_date: targetDate, day_start_hour: dayStartHour, scheduled_time_ms: scheduledMs });
     try {
       const aggregate = await aggregateStudyUsageForTargetDate(env, targetDate);
-      console.log("APP_USAGE_SCHEDULED_AGGREGATION_DONE", { target_date: targetDate, study_minutes: aggregate.minutes, study_sessions: aggregate.sessions, study_last_used_at: aggregate.last });
+      console.log("APP_USAGE_SCHEDULED_AGGREGATION_DONE", { target_date: targetDate, day_start_hour: dayStartHour, study_minutes: aggregate.minutes, study_sessions: aggregate.sessions, study_last_used_at: aggregate.last });
     } catch (error) {
       const msg = error instanceof Error ? error.message : String(error);
-      console.error("APP_USAGE_SCHEDULED_AGGREGATION_ERROR", { target_date: targetDate, message: msg });
+      console.error("APP_USAGE_SCHEDULED_AGGREGATION_ERROR", { target_date: targetDate, day_start_hour: dayStartHour, message: msg });
       throw error;
     }
   },
